8f831ba805fe654a8ef98f83f88fdee5cfec5ea0
donot1 <kalghatgin@gmail.com>
2021-05-25 22:52:59 -0500

    variable size tasks test

---
+++ tests/pool/variableSizeTasksC.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+#include "ThreadPoolForC.hpp"
+#include "workForC.hpp"
+#include "Architecture.hpp"
+#include "Scheduler.hpp"
+
+int main (int argc, char *argv[]){
+
+  /*
+   * Fetch the inputs.
+   */
+  if (argc < 4){
+    std::cerr << "USAGE: " << argv[0] << " TASKS MAX_ITERS THREADS" << std::endl;
+    return 1;
+  }
+  auto tasks = atoi(argv[1]);
+  auto max_iters = atoi(argv[2]);
+  auto threads = atoi(argv[3]);
+
+  /*
+   * Create the scheduler.
+   */
+  MARC::ThreadPoolForC pool(threads);
+  const MARC::Architecture arch;
+  MARC::Scheduler scheduler(pool, arch);
+
+
+  /*
+   * Submit jobs.
+   * Number of iterations per task is *almost* uniformly chosen from [1, MAX_ITERS]
+   */
+  for (auto i=0; i < tasks; i++){
+    auto iters = rand() % max_iters + 1;
+    scheduler.submitAndDetach(myF, (void*)iters, 1, 0);
+  }
+
+  return 0;
+}
